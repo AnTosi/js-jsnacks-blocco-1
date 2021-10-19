32936ccbd7e274926b920b8304883d882b3412a2
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-19 10:38:59 +0200

    aggiunto come commento nel 7 il motivo per cui faceva ciclo infinito

--- JSnack7/main.js
+++ JSnack7/main.js
@@ -16,3 +16,15 @@ while (Math.pow(2,n) < 1000) {
 }
 
 // il browser non me lo carica, non so se perché sbaglio io o perché è il browser
+
+
+//in questo modo non funziona, mi cicla all'infinito, credo che il problema sia nel fatto che mi calcola la n (e quindi la condizione) fuori dal ciclo quindi il ciclo non riceve mai la condizione false, perché non la calcola mai
+// var n = 0;
+// let elevated = Math.pow(2,n);
+
+// while (elevated < 1000) {
+    
+//     // let elevated = Math.pow (2, n);
+//         document.querySelector("h1").insertAdjacentHTML("beforeend", ` ${elevated}`);
+//         n++;
+// }
